149454cdd2ba3ecf056dbd7e4a8f80c6cfd2f4f0
peiwen.liu <peiwen.liu@picovr.com>
2020-03-23 16:31:39 +0800

    update IProxy

--- pwplugin/src/main/java/cn/qd/peiwen/plugin/IProxy.java
+++ pwplugin/src/main/java/cn/qd/peiwen/plugin/IProxy.java
@@ -2,12 +2,11 @@ package cn.qd.peiwen.plugin;
 
 
 public interface IProxy {
-
     boolean isResumed();
-
-    void onComponentPrepared();
 
     void onComponentPrepareing();
 
-    void onComponentResourceWaiting();
+    void onComponentPrepareFailured();
+
+    void onComponentPrepareSuccessed();
 }
